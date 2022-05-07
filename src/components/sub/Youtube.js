246aacd027d249dfc0947b83df26469a7b1d3cc8
tubi55 <tubi55@nate.com>
2022-05-07 13:11:56 +0900

    fecth로 받아진 데이터를 리듀서에 전달

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,29 +1,41 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setYoutube } from '../../redux/actions';
 import axios from 'axios';
 import Layout from '../common/Layout';
 import Popup from '../common/Popup';
 
 function Youtube() {
+	//store에 youtubeReuder데이터를 가져옴 (빈배열)
+	const vidData = useSelector((store) => store.youtubeReducer.youtube);
+	//dispatch 전송함수 활성화
+	const dispatch = useDispatch();
 	const pop = useRef(null);
-	const [items, setItems] = useState([]);
+
 	const [index, setIndex] = useState(0);
 	const [loading, setLoading] = useState(false);
 
-	const api_key = 'AIzaSyCCiJkX1nNqYL222H5m-0fCS65LfzyExlQ';
-	const play_list = 'PLHtvRFLN5v-UVVpNfWqtgZ6YPs9ZJMWRK';
-	const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&playlistId=${play_list}&maxResults=3&part=snippet`;
+	//axios 데이타 호출 구문을 함수로 패키징
+	const fetchYoutube = async () => {
+		const key = 'AIzaSyCCiJkX1nNqYL222H5m-0fCS65LfzyExlQ';
+		const id = 'PLHtvRFLN5v-UVVpNfWqtgZ6YPs9ZJMWRK';
+		const url = `https://www.googleapis.com/youtube/v3/playlistItems?key=${key}&playlistId=${id}&maxResults=3&part=snippet`;
 
-	useEffect(() => {
-		axios.get(url).then((json) => {
-			setItems(json.data.items);
+		//해당 함수에서 axios로 유튜브 데이터를 받아오면 받아온 정보값을 dispatch함수로 다시 reducer에 전달
+		await axios.get(url).then((json) => {
+			dispatch(setYoutube(json.data.items));
 			setLoading(true);
 		});
+	};
+
+	useEffect(() => {
+		fetchYoutube();
 	}, []);
 
 	return (
 		<>
 			<Layout name={'Youtube'}>
-				{items.map((item, idx) => {
+				{vidData.map((item, idx) => {
 					let desc = item.snippet.description;
 					let desc_len = desc.length;
 					let date = item.snippet.publishedAt;
@@ -53,7 +65,7 @@ function Youtube() {
 					<iframe
 						src={
 							'https://www.youtube.com/embed/' +
-							items[index].snippet.resourceId.videoId
+							vidData[index].snippet.resourceId.videoId
 						}
 						frameBorder='0'></iframe>
 				)}
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,13 @@
-//인수로 전달된 값을 type이 SET_MEMBERS인 액션 객체를 반환하는 함수
 export const setMembers = (member) => {
 	return {
 		type: 'SET_MEMBERS',
 		payload: member,
+	};
+};
+
+export const setYoutube = (data) => {
+	return {
+		type: 'SET_YOUTUBE',
+		payload: data,
 	};
 };
