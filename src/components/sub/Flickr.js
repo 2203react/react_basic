84dc27bda91a8fdd4334f3860c391f97af7eda3a
tubi55 <tubi55@nate.com>
2022-05-14 13:35:47 +0900

    모든 데이터가 호출완료되면 동기적으로 리스트 보이도록 수정

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -1,19 +1,22 @@
 import Layout from '../common/Layout';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
 function Flickr() {
+	const frame = useRef(null);
 	const [items, setItems] = useState([]);
 
 	const fetchFlickr = async () => {
 		const api_key = 'feb5dbb632085ee9e53c197d363d1a85';
-		const method = 'flickr.interestingness.getList';
-		const per_page = 50;
-		const url = `https://www.flickr.com/services/rest/?method=${method}&api_key=${api_key}&format=json&nojsoncallback=1&per_page=${per_page}`;
+		const method_interest = 'flickr.interestingness.getList';
+		const per_page = 500;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&format=json&nojsoncallback=1&per_page=${per_page}`;
 
 		await axios.get(url).then((json) => {
 			setItems(json.data.photos.photo);
 		});
+		//await 데이터호출이 완료되면 frame보임 처리
+		frame.current.classList.add('on');
 	};
 
 	useEffect(() => {
@@ -22,7 +25,7 @@ function Flickr() {
 
 	return (
 		<Layout name={'Flickr'}>
-			<div className='frame on'>
+			<div className='frame' ref={frame}>
 				{items.map((item, idx) => {
 					return (
 						<article key={idx}>
