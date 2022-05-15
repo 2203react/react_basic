1c2b57134cd3061410f52dbc85ed09ed2b389f4a
tubi55 <tubi55@nate.com>
2022-05-15 13:20:34 +0900

    팝업 연결

--- src/components/sub/Flickr.js
+++ src/components/sub/Flickr.js
@@ -1,4 +1,5 @@
 import Layout from '../common/Layout';
+import Popup from '../common/Popup';
 import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import Masonry from 'react-masonry-component';
@@ -7,8 +8,10 @@ const path = process.env.PUBLIC_URL;
 function Flickr() {
 	const frame = useRef(null);
 	const input = useRef(null);
+	const pop = useRef(null);
 	const [items, setItems] = useState([]);
 	const [loading, setLoading] = useState(true);
+	const [index, setIndex] = useState(0);
 	const [enableClick, setEnableClick] = useState(true);
 
 	const masonryOptions = {
@@ -94,70 +97,89 @@ function Flickr() {
 	}, []);
 
 	return (
-		<Layout name={'Flickr'}>
-			<button onClick={showInterest}>interest</button>
+		<>
+			<Layout name={'Flickr'}>
+				<button onClick={showInterest}>interest</button>
 
-			<div className='searchBox'>
-				<input
-					type='text'
-					ref={input}
-					onKeyUp={(e) => {
-						if (e.key === 'Enter') showSearch();
-					}}
-				/>
-				<button onClick={showSearch}>search</button>
-			</div>
+				<div className='searchBox'>
+					<input
+						type='text'
+						ref={input}
+						onKeyUp={(e) => {
+							if (e.key === 'Enter') showSearch();
+						}}
+					/>
+					<button onClick={showSearch}>search</button>
+				</div>
 
-			{loading ? (
-				<img src={path + '/img/loading.gif'} className='loading' />
-			) : null}
+				{loading ? (
+					<img src={path + '/img/loading.gif'} className='loading' />
+				) : null}
 
-			<div className='frame' ref={frame}>
-				<Masonry elementType={'div'} options={masonryOptions}>
-					{items.map((item, idx) => {
-						return (
-							<article key={idx}>
-								<div className='inner'>
-									<div className='pic'>
-										<img
-											src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
-										/>
-									</div>
-									<h2>{item.title}</h2>
-
-									<div className='profile'>
-										<img
-											src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
-											onError={(e) => {
-												e.target.setAttribute(
-													'src',
-													'https://www.flickr.com/images/buddyicon.gif'
-												);
-											}}
-										/>
-										<span
-											onClick={(e) => {
-												if (enableClick) {
-													setEnableClick(false);
-													setLoading(true);
-													frame.current.classList.remove('on');
-													fetchFlickr({
-														type: 'user',
-														count: 100,
-														user: e.currentTarget.innerText,
-													});
-												}
+				<div className='frame' ref={frame}>
+					<Masonry elementType={'div'} options={masonryOptions}>
+						{items.map((item, idx) => {
+							return (
+								<article key={idx}>
+									<div className='inner'>
+										<div
+											className='pic'
+											onClick={() => {
+												pop.current.open();
+												setIndex(idx);
 											}}>
-											{item.owner}
-										</span>
+											<img
+												src={`https://live.staticflickr.com/${item.server}/${item.id}_${item.secret}_m.jpg`}
+											/>
+										</div>
+										<h2>{item.title}</h2>
+
+										<div className='profile'>
+											<img
+												src={`http://farm${item.farm}.staticflickr.com/${item.server}/buddyicons/${item.owner}.jpg`}
+												onError={(e) => {
+													e.target.setAttribute(
+														'src',
+														'https://www.flickr.com/images/buddyicon.gif'
+													);
+												}}
+											/>
+											<span
+												onClick={(e) => {
+													if (enableClick) {
+														setEnableClick(false);
+														setLoading(true);
+														frame.current.classList.remove('on');
+														fetchFlickr({
+															type: 'user',
+															count: 100,
+															user: e.currentTarget.innerText,
+														});
+													}
+												}}>
+												{item.owner}
+											</span>
+										</div>
 									</div>
-								</div>
-							</article>
-						);
-					})}
-				</Masonry>
-			</div>
-		</Layout>
+								</article>
+							);
+						})}
+					</Masonry>
+				</div>
+			</Layout>
+
+			<Popup ref={pop}>
+				{items.length !== 0 ? (
+					<img
+						src={`https://live.staticflickr.com/${items[index].server}/${items[index].id}_${items[index].secret}_b.jpg`}
+					/>
+				) : null}
+
+				<span className='close' onClick={() => pop.current.close()}>
+					close
+				</span>
+			</Popup>
+		</>
 	);
 }
 
