5c272f6fd4fa84c6210e2a3ab569a821f12e7837
tubi55 <tubi55@nate.com>
2022-05-21 13:33:54 +0900

    예외처리

--- src/redux/saga.js
+++ src/redux/saga.js
@@ -2,9 +2,12 @@ import { takeLatest, all, put, fork, call } from 'redux-saga/effects';
 import { fetchFlickr } from './api';
 
 export function* returnFlickr(action) {
-	console.log(action);
-	const response = yield call(fetchFlickr, action.opt);
-	yield put({ type: 'FLICKR_SUCCESS', payload: response.data.photos.photo });
+	try {
+		const response = yield call(fetchFlickr, action.opt);
+		yield put({ type: 'FLICKR_SUCCESS', payload: response.data.photos.photo });
+	} catch (err) {
+		yield put({ type: 'FLICKR_ERROR', payload: err });
+	}
 }
 
 export function* callFlickr() {
