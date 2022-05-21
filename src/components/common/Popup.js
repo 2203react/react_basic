7e930f36ae4111c8b48d2647dc6d810349a12857
tubi55 <tubi55@nate.com>
2022-05-21 15:21:28 +0900

    메인 페이지에 팝업 연결

--- src/components/common/Popup.js
+++ src/components/common/Popup.js
@@ -23,13 +23,12 @@ const Popup = forwardRef((props, ref) => {
 	}, [open]);
 
 	return (
-		// 해당 컴포넌트가 unmount시 사라지는 모션이 끝난 뒤에 DOM제거
 		<AnimatePresence>
 			{open && (
 				<motion.aside
-					initial={{ opacity: 0, scale: 0 }} //모션이 일어나기전 초기값 설정
-					animate={{ opacity: 1, scale: 1, transition: { duration: 0.5 } }} //mount시 동작될 값 설정
-					exit={{ opacity: 0, scale: 0 }} //unmount시 동작될 값 설정
+					initial={{ opacity: 0, scale: 0 }}
+					animate={{ opacity: 1, scale: 1, transition: { duration: 0.5 } }}
+					exit={{ opacity: 0, scale: 0 }}
 					className='popup'>
 					<motion.div
 						initial={{ opacity: 0 }}
--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,23 +1,47 @@
 import { useSelector } from 'react-redux';
+import { useState, useRef } from 'react';
+import Popup from '../common/Popup';
 
 function Pics() {
 	const { flickr } = useSelector((store) => store.flickrReducer);
+	const [index, setIndex] = useState(0);
+	const pop = useRef(null);
 	return (
-		<section id='pics' className='myScroll'>
-			<h1>Recent Gallery</h1>
+		<>
+			<section id='pics' className='myScroll'>
+				<h1>Recent Gallery</h1>
 
-			{flickr.map((pic, idx) => {
-				if (idx < 5) {
-					return (
-						<li key={idx}>
-							<img
-								src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_s.jpg`}
-							/>
-						</li>
-					);
-				}
-			})}
-		</section>
+				{flickr.map((pic, idx) => {
+					if (idx < 5) {
+						return (
+							<li
+								key={idx}
+								onClick={() => {
+									setIndex(idx);
+									pop.current.open();
+								}}>
+								<img
+									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_s.jpg`}
+								/>
+							</li>
+						);
+					}
+				})}
+			</section>
+
+			<Popup ref={pop}>
+				{flickr.length !== 0 && (
+					<>
+						<img
+							src={`https://live.staticflickr.com/${flickr[index].server}/${flickr[index].id}_${flickr[index].secret}_b.jpg`}
+						/>
+						<span className='close' onClick={() => pop.current.close()}>
+							close
+						</span>
+					</>
+				)}
+			</Popup>
+		</>
 	);
 }
 
