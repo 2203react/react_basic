ac3497c413765e142e11aa5a9f7d500480997fa1
tubi55 <tubi55@nate.com>
2022-05-22 14:20:28 +0900

    모바일 패널 생성

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,34 +1,49 @@
 import { NavLink } from 'react-router-dom';
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
-function Menu() {
+const Menu = forwardRef((props, ref) => {
+	const [open, setOpen] = useState(false);
+
+	useImperativeHandle(ref, () => {
+		return {
+			open: () => setOpen(true),
+			close: () => setOpen(false),
+		};
+	});
+
 	return (
-		<nav className='menuM'>
-			<h1>
-				<NavLink to='/'>LOGO</NavLink>
-			</h1>
+		<AnimatePresence>
+			{open && (
+				<nav className='menuM'>
+					<h1>
+						<NavLink to='/'>LOGO</NavLink>
+					</h1>
 
-			<ul id='gnbMo'>
-				<li>
-					<NavLink to='/department'>Department</NavLink>
-				</li>
-				<li>
-					<NavLink to='/community'>Community</NavLink>
-				</li>
-				<li>
-					<NavLink to='/flickr'>Flickr</NavLink>
-				</li>
-				<li>
-					<NavLink to='/youtube'>Youtube</NavLink>
-				</li>
-				<li>
-					<NavLink to='/location'>Location</NavLink>
-				</li>
-				<li>
-					<NavLink to='/join'>Join</NavLink>
-				</li>
-			</ul>
-		</nav>
+					<ul id='gnbMo'>
+						<li>
+							<NavLink to='/department'>Department</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community'>Community</NavLink>
+						</li>
+						<li>
+							<NavLink to='/flickr'>Flickr</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube'>Youtube</NavLink>
+						</li>
+						<li>
+							<NavLink to='/location'>Location</NavLink>
+						</li>
+						<li>
+							<NavLink to='/join'>Join</NavLink>
+						</li>
+					</ul>
+				</nav>
+			)}
+		</AnimatePresence>
 	);
-}
+});
 
 export default Menu;
