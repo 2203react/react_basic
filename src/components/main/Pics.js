81ccff1c3bea74f2d2d4f8849eef18299c3224de
tubi55 <tubi55@nate.com>
2022-05-22 13:13:22 +0900

    pallax 기능 적용

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -2,14 +2,40 @@ import { useSelector } from 'react-redux';
 import { useState, useRef } from 'react';
 import Popup from '../common/Popup';
 
-function Pics() {
+function Pics(props) {
+	console.log(props.scrolled);
 	const { flickr } = useSelector((store) => store.flickrReducer);
 	const [index, setIndex] = useState(0);
 	const pop = useRef(null);
+	//현재 스크롤되는 거리값
+	const scrolled = props.scrolled;
+	//해당 섹션의 세로 위치값
+	const start = props.start;
+	//양수: 기준점을 위로 끌어올림, 음수: 기준점을 아래로 내림
+	const base = 400;
+	//scrolled값 적용시점에 위의 보정값 적용
+	const position = scrolled - start + base;
+
 	return (
 		<>
 			<section id='pics' className='myScroll'>
-				<h1>Recent Gallery</h1>
+				<h1
+					style={
+						position >= 0
+							? { transform: `translateX(${position * 1.5}px)` }
+							: null
+					}>
+					Recent Gallery
+				</h1>
+
+				<h2
+					style={
+						position >= 0
+							? { transform: `translateX(${position * 1}px)` }
+							: null
+					}>
+					Gallery
+				</h2>
 
 				{flickr.map((pic, idx) => {
 					if (idx < 5) {
